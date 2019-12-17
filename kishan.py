891361c71c94d8068c61f7d2b0ec35670c871074
your name <41380677+kishanrocks7@users.noreply.github.com>
2019-12-17 19:20:22 +0530

    opened the camers using VideoCapture()

---
+++ kishan.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:cd  
+    ret , frame=cap.read()
+
+    cv2.imshow('frame', frame)
+    cv2.imshow('frame1', frame)
+    cv2.imshow('frame2', frame)
+    cv2.imshow('frame3', frame)
+    cv2.imshow('frame4', frame)  #imgshow
+    if cv2.waitKey(20) & 0xFF==ord('q'):
+        break
+;lsf
+
+divmodfsd
+fds
+fsd
+f
+sd
